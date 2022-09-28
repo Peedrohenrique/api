f98fffac0cb100d99bf3cbb5fb2895465e8d2a24
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-09-27 21:22:28 -0300

    Cors implementado

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = process.env.DB_PORT || 8089;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  app.use(cors());
+  next();
+});
 // Import Routes
 const usersRoutes = require("./routes/usersRoutes");
 
 // Routes
 app.use(usersRoutes);
+
+const PORT = process.env.DB_PORT || 8089;
 
 app.listen(PORT, () => {
   console.log(`API RODANADO NA PORTA ${PORT}`);
